8c34a3374925abe53d5caa8c62e8b3ff239a8031
Triangle717 <le717@hotmail.com>
2013-07-04 09:53:07 -0400

    Update feed.js
    Make links have target="_blank" (thanks to Sim533!)

--- pages/feed.js
+++ pages/feed.js
@@ -1,25 +1,25 @@
 /*
 *  How to use the Feed Control to grab, parse and display feeds.
 */
-
+    
 google.load("feeds", "1");
-
+    
 function OnLoad() {
-  // Create a feed control
-  var feedControl = new google.feeds.FeedControl();
+    // Create a feed control
+    var feedControl = new google.feeds.FeedControl();
+    
+    // Add the feed
+    feedControl.addFeed("http://www.thedailybrick.co.uk/blog/feed");      
+    
+     // Draw it
+    feedControl.draw(document.getElementById("content"));
 
-  // Add two feeds.
-  feedControl.addFeed("http://www.thedailybrick.co.uk/blog/feed");
-
-  // if more than one with a class
-  $("a.gf-title").each(function() {
-      $(this).attr("target","_blank");
-  });
-
-  // Draw it.
-  feedControl.draw(document.getElementById("content"));
-}
+    // if more than one with a class
+    $("a.gf-title").each(function() {
+        $(this).attr("target","_blank");
+    });
+  }
+   
+  google.setOnLoadCallback(OnLoad);
 
 
-
-google.setOnLoadCallback(OnLoad);
