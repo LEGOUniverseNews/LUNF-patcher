314239f8e9566d3addf07a793f8ae6d426a89a08
Triangle717 <le717@hotmail.com>
2013-12-30 17:01:45 -0500

    Go back to jQuery 1.10, use <base> tag for opening new links as _blank,
    Simplify YT video changing, don't use jQuery to inject src attribute
    into player iframe

--- js/script.js
+++ js/script.js
@@ -10,26 +10,24 @@
 */
 
 $(document).ready(function() {
-    // Open all links in new tabs
-    $("a").attr("target", "_blank");
-
     /* YouTube embed player of current LEGO-related video
     preferably from the LEGO channel (http://www.youtube.com/user/LEGO
     */
 
     /* FUTURE When updating the YouTube video, always get the code directly after
     "watch?v=". For example, if you wanted to embed "http://www.youtube.com/watch?v=g8-4wXkT60c",
-    you would take "g8-4wXkT60c" and place it after "embed/".
-    Thus the new link would like "http://www.youtube.com/embed/g8-4wXkT60c"
+    you would take "g8-4wXkT60c" and paste it between the double quotess.
+    Thus the new value of "ytVideo" would be "g8-4wXkT60c".
     */
-    var ytVideo = "http://www.youtube.com/embed/YEtjhWwUv2g";
+    var ytVideo = "YEtjhWwUv2g";
 
-    /* Player parameters. Full list and definitions at
+    /* YouTube domain and player parameters. Full list and definitions at
     https://developers.google.com/youtube/player_parameters#Parameters */
+    var ytDomain = "http://www.youtube.com/embed/";
     var ytVideoParams = "?html5=1&rel=0&autohide=1&modestbranding=1";
 
     // Give the iframe an src attribute of the new video
-    $("#yt-video").attr("src", ytVideo + ytVideoParams);
+    document.getElementById("yt-video").src = ytDomain + ytVideo + ytVideoParams;
 });
 
 //    // Activate scroll bar
