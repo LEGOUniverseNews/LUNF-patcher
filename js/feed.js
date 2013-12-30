d8b9b818b6d79aa55659cb63d03407ef311ccdad
Triangle717 <le717@hotmail.com>
2013-12-30 13:51:39 -0500

    px to em, move FeedEx to feed.js

---
+++ js/feed.js
@@ -0,0 +1,19 @@
+/*
+    LEGO Universe News! Patcher
+
+    Created 2013-2014 Triangle717 & rioforce
+    <http://Triangle717.WordPress.com/>
+    <http://rioforce.WordPress.com/>
+
+    Licensed under The MIT License
+    <http://opensource.org/licenses/MIT>
+*/
+
+$('#news-feed').FeedEk({
+    FeedUrl: "http://legouniversenews.wordpress.com/feed/",
+    MaxCount: 3,
+    ShowDesc: true,
+    ShowPubDate: false,
+    DescCharacterLimit :155,
+    TitleLinkTarget: "_blank"
+});
